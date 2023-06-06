3b4e8fd6dcc7c9871acaa6794d9e0d6702ef1f3c
Micheala-H <mholthus17@gmail.com>
2023-06-05 23:23:37 -0500

    added HTML code and started JS code for API

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-// const APIKey = "";
+const APIKey = "480acf8157b90eef500fa76bc0ac0586";
 
-// $(function() {
-//     cities();
+$(function() {
+    searchCity();
+    //http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+})
+
+$(function() {
+    searchWeather();
+    //api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
     
-// })
+})
